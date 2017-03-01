b01096aac42cf5665a26b495b22563b88862b49d
Ankur Chakroborty <chakrobo@adobe.com>
2017-03-01 19:08:53 +0530

    Implement Search Suggestions service

--- src/main/java/com/adeo/connector/opus/service/OpusGatewayService.java
+++ src/main/java/com/adeo/connector/opus/service/OpusGatewayService.java
@@ -354,6 +354,15 @@ public interface OpusGatewayService {
      * @return				ContentSet instance containing all the results.
      */
     <T> ContentSet<T> findSeries(String keyword, SearchFilterType filterType, Class<T> modelClass);
+    
+    /**
+     * Find search suggestions based on keywords.
+     * OSGi Configuration pattern: com.adeo.connector.opus.SearchSuggetionRequest:/search/v2/suggest/phrase?input={0}&field={1}&size={2}:ContentSetProcessor
+     * @param keyword		The keyword to Search for.
+     * @param modelClass	The model class used to parse the OPUS response.
+     * @return				ContentSet instance containing all the results.
+     */
+    <T> ContentSet<T> getSearchSuggestions(String keyword, Class<T> modelClass);
 
 
 
